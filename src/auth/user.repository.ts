5a6ee2d5be78907aaa4dbb8dbb92611872756a7f
Bùi Quốc Tĩnh <tinhbui.htn2020@gmail.com>
2024-06-21 11:08:11 +0700

    validator AuthCredentialDto, catch confict user

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,10 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
@@ -9,6 +13,14 @@ export class UserRepository extends Repository<User> {
       username,
       password,
     });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
