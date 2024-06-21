478b0fbc27dc98a673131a53d5bdef341bb47254
Bùi Quốc Tĩnh <tinhbui.htn2020@gmail.com>
2024-06-21 11:13:37 +0700

    hash password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,13 +5,19 @@ import {
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
+
+    // hash password
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
     try {
       await this.save(user);
