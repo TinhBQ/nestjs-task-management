532a25fd64d016867140123ad57fc25182ff885b
Bùi Quốc Tĩnh <tinhbui.htn2020@gmail.com>
2024-06-21 10:59:17 +0700

    singup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,14 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialDto } from './dto/auth-credential.dto';
 
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async createUser(authCredentialDto: AuthCredentialDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+    const user = this.create({
+      username,
+      password,
+    });
+    await this.save(user);
+  }
+}
