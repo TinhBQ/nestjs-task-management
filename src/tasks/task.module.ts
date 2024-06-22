09e5b2737ede153427bc38da7de9944d814c3ae4
Bùi Quốc Tĩnh <tinhbui.htn2020@gmail.com>
2024-06-22 14:03:43 +0700

    add `AuthGuard`

--- src/tasks/task.module.ts
+++ src/tasks/task.module.ts
@@ -4,9 +4,10 @@ import { TaskService } from './task.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
 import { Task } from './task.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Task])],
+  imports: [TypeOrmModule.forFeature([Task]), AuthModule],
   controllers: [TaskController],
   providers: [TaskService, TaskRepository],
 })
